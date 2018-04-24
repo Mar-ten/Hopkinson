7de7ad504ac72a175a95dc4170e2bd180f828048
Nik <nik.benko17@gmail.com>
2018-04-24 16:33:14 -0600

    updates to matlab

--- Matlab/Time_Shift_Test.m
+++ Matlab/Time_Shift_Test.m
@@ -1,6 +1,8 @@
 % Time Shifting
+close all
+clear all
 
-filename=['Group4_Test',num2str(4),'.csv'];
+filename=['Group4_Test',num2str(8),'.csv'];
 data=importdata(filename,',',16);
 time=data.data(:,1);
 v_incident=data.data(:,2);
@@ -14,9 +16,19 @@ dt=8.0000e-09;
 time_shift=L/speed;
 index_shift=round(time_shift/dt);
 
+
+%% Pad and Shift Data
 v_forward=[zeros(index_shift,1);v_incident];
 v_back=[v_incident;zeros(index_shift,1)];
+v_diff=v_forward+v_back;
+%% Trim Data
+Lcut=round(length(v_diff)/3);
 
-plot((1:length(v_forward))*dt,v_forward,(1:length(v_back))*dt,v_back)
-hold on
-plot((1:length(v_forward))*dt,v_forward+v_back)
+v_forward(1:Lcut)=[]; v_forward(end-Lcut:end)=[];
+v_back(1:Lcut)=[]; v_back(end-Lcut:end)=[];
+v_diff(1:Lcut)=[]; v_diff(end-Lcut:end)=[];
+
+%% Plots
+
+plot((1:length(v_forward))*dt,v_forward,(1:length(v_back))*dt,v_back,(1:length(v_back))*dt,v_diff)
+legend('Incident','Reflected','Difference');
